a31132a75a7cc8c27e6ac19f429c44c4feabf483
Komal <ks989156@gmail.com>
2025-02-21 20:34:41 +0530

    added Transcription

---
+++ src/Components/Transcription.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+function Transcription(props) {
+
+    const {textElement} = props
+
+    return (
+    <div>
+      {textElement}
+    </div>
+  )
+}
+
+export default Transcription
